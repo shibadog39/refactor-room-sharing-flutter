cbc1905639a8eaeeabf7ac6ec3733d0dad495c31
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-21 09:33:37 +0900

    精算テーブルの表示を進めた ・cardの使用 ・tableの使用 ・container等の復習

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,74 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final bottomButtons = new Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Icon(Icons.lightbulb_outline),
+                new Text('Idea'),
+              ],
+            ),
+          ),
+          FlatButton(
+            onPressed: () {},
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Icon(Icons.lightbulb_outline),
+                new Text('Idea'),
+              ],
+            ),
+          ),
+          FlatButton(
+            onPressed: () {},
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Icon(Icons.lightbulb_outline),
+                new Text('Idea'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final results = Container(
+        padding: EdgeInsets.all(20),
+        child: Card(
+          child: Table(
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: BorderSide(
+                              width: 1.0, color: Color(0xFFFF000000)))),
+                  children: [
+                    Text('名前'),
+                    Text('負担'),
+                    Text('精算'),
+                  ]),
+              TableRow(children: [
+                Text('櫻井'),
+                Text('¥120'),
+                Text('+ ¥60'),
+              ]),
+              TableRow(children: [
+                Text('櫻井'),
+                Text('¥0'),
+                Text('- ¥60'),
+              ]),
+            ],
+          ),
+        ));
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -16,60 +84,28 @@ class MyApp extends StatelessWidget {
           title: Text('Refactor Room Sharing'),
         ),
         body: Center(
-          child: Column(
+            child: Container(
+          height: 600,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '0',
-                style: Theme.of(context).textTheme.display1,
+            children: [
+              Container(
+                width: 300,
+                child: Container(
+                  child: Column(
+                    children: [results],
+                  ),
+                ),
               ),
             ],
           ),
-        ),
+        )),
         floatingActionButton: FloatingActionButton(
           tooltip: 'Increment',
           child: Icon(Icons.add),
         ),
-        bottomNavigationBar: new Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              FlatButton(
-                onPressed: () {},
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    new Icon(Icons.lightbulb_outline),
-                    new Text('Idea'),
-                  ],
-                ),
-              ),
-              FlatButton(
-                onPressed: () {},
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    new Icon(Icons.lightbulb_outline),
-                    new Text('Idea'),
-                  ],
-                ),
-              ),
-              FlatButton(
-                onPressed: () {},
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    new Icon(Icons.lightbulb_outline),
-                    new Text('Idea'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+        bottomNavigationBar: bottomButtons,
       ),
     );
   }
